460092471e6c368e58e6e6e94d4d02fe292f0600
Jesper <Yeahsper@gmail.com>
2020-02-06 10:44:39 +0100

    Program done

--- JUnitTest/src/main/Main.java
+++ JUnitTest/src/main/Main.java
@@ -9,7 +9,9 @@ public class Main {
 		
 		System.out.println("Enter two values to add them together");
 		
+		System.out.print("Value 1: ");
 		int number1 = scan.nextInt();
+		System.out.print("Value 2: ");
 		int number2 = scan.nextInt();
 		int answer = number1+number2;
 		System.out.println("The answer is: " + answer);
